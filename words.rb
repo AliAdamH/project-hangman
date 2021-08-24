7a0b65a153b8ef4af68097704275e0feec674c19
Ali Hassaine <ali.hs@outlook.fr>
2021-08-24 18:22:54 +0000

    Add hiding and mapping methods

--- words.rb
+++ words.rb
@@ -9,7 +9,24 @@ module Hangman
 
     def random_word
       line = rand(52_452)
-      IO.readlines('better_dict.txt', chomp: true)[line]
+      IO.readlines('better_dict.txt', chomp: true)[line].downcase
+    end
+
+    def hidden
+      @word.gsub(/[a-z]/, ' _ ')
+    end
+
+    def mapping
+      @word.split('').each_with_index.reduce({}) do |acc, (letter, index)|
+        if acc[letter].is_a?(Array)
+          acc[letter] << index
+        elsif acc.key?(letter)
+          acc[letter] = [acc[letter]] << index
+        else
+          acc[letter] = index
+        end
+        acc
+      end
     end
   end
 end
