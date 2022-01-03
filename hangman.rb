ef591a634d9c91d3ec38eca082a2af4f1ac77bba
Ali Hassaine <ali.hs@outlook.fr>
2022-01-03 22:26:32 +0000

    refactor pust and choice selection

--- hangman.rb
+++ hangman.rb
@@ -1,36 +1,28 @@
-require './core_extensions.rb'
-require './words.rb'
-require './game.rb'
+require './core_extensions'
+require './words'
+require './game'
 require 'yaml'
 
-loop do 
-
+loop do
   puts <<~INTRO
     Welcome to the Hangman Game !
     To play a new game enter: 1
     To load the last saved game enter: 2
   INTRO
   starting_choice = gets.chomp
-  if (starting_choice == "1")
-    Hangman::Game.new.play
-  elsif (starting_choice == "2")
-    Hangman::Game.load_game.play
+  case starting_choice
+  when '1' then Hangman::Game.new.play
+  when '2' then Hangman::Game.load_game.play
   else
     puts 'You\'ve entered an invalid input'.red
     next
   end
 
-  puts "Would you like to play another game ? ".yellow
-  puts "To play a another game enter: 1"
-  puts "To quit the game enter: 2"
-  restart_choice = gets.chomp.to_i
-  if (restart_choice == 1)
-    next
-  elsif(restart_choice == 2)
-    break
-  else
-    puts 'You\'ve entered an invalid input'.red
-  end
+  puts <<~OUTRO
+    #{'Would you like to play another game ?'.yellow}
+    To play a another game enter: 1
+    To quit the game enter any other key
+  OUTRO
+  restart_choice = gets.chomp
+  restart_choice == '1' ? next : break
 end
-
-
